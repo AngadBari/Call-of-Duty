3ef5c946259edaf90679eb3da5f7f8a51588d03b
AngadBari <angadbari2@gmail.com>
2025-12-07 14:51:57 +0530

    Hero-GSAP Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef,useEffect } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -15,7 +23,63 @@ const Hero = () => {
   const handleMinVideoPlayer = () => {
     setHasClick(true);
     setCurrentIndex(upcomingVideo);
+
   };
+
+  useEffect(() => {
+    if (loadedVideo === totalVideos - 1) {
+      setIsLoading(false);
+    }
+  }, [loadedVideo]);
+
+
+   useGSAP(
+    () => {
+      if (hasClick) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVideoRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
+
 
   const getVideoSrc = (index) => `videos/Call-${index}.mp4`;
 
@@ -25,12 +89,22 @@ const Hero = () => {
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
         <div>
-          {/* Small Preview Video */}
+         
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div
               onClick={handleMinVideoPlayer}
@@ -48,7 +122,7 @@ const Hero = () => {
             </div>
           </div>
 
-          {/* Middle Invisible Video */}
+         
           <video
             ref={nextVideoRef}
             src={getVideoSrc(currentIndex)}
@@ -59,7 +133,7 @@ const Hero = () => {
             onLoadedData={handleVideoLoad}
           />
 
-          {/* Background Main Video */}
+        
           <video
             src={getVideoSrc(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
@@ -80,9 +154,24 @@ const Hero = () => {
             <div className=" mt-24 px-5 sm:px-10">
               <h1 className="special-font hero-heading  text-blue-75"><b>W</b>ar<b>Z</b>one</h1>
               
+              <p className="mb-5 max-w-64 font-mono text-blue-100">Every Bullet Counts. Every Soldier Matters.</p>
+
+              <Button
+                id="watch-tailer" 
+                title="Watch-Tailer" 
+                leftIcon={<TiLocationArrow/>}
+                containerClass="bg-yellow-300 flex-center gap-1"
+                
+              />
             </div>
           </div>
       </div>
+      
+      <h1 
+      className=" special-font hero-heading  absolute bottom-5 right-5  text-black">
+          <b>Call of Duty</b>
+          </h1>
+
     </div>
   );
 };
